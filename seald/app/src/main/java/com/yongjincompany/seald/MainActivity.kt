b3ad7735208bc3812bae009c700d5f6ba78c3a16
Leesin0222 <dragon02229@naver.com>
2021-12-23 11:35:49 +0900

    📑 :: 눈덩이 구현

--- seald/app/src/main/java/com/yongjincompany/seald/MainActivity.kt
+++ seald/app/src/main/java/com/yongjincompany/seald/MainActivity.kt
@@ -1,10 +1,15 @@
 package com.yongjincompany.seald
 
+import android.content.Intent
+import android.graphics.Color
 import android.graphics.drawable.AnimationDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_world.*
+import nl.dionsegijn.konfetti.models.Shape
+import nl.dionsegijn.konfetti.models.Size
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,5 +23,21 @@ class MainActivity : AppCompatActivity() {
         animDrawable.setExitFadeDuration(5000)
         animDrawable.start()
 
+
+        viewKonfetti.build()
+            .addColors(Color.WHITE, Color.WHITE, Color.WHITE)
+            .setDirection(0.0, 359.0)
+            .setSpeed(1f, 5f)
+            .setFadeOutEnabled(true)
+            .setTimeToLive(2000L)
+            .addShapes(Shape.Circle, Shape.Circle)
+            .addSizes(Size(12))
+            .setPosition(-50f, viewKonfetti.width + 50f, -50f, -50f)
+            .streamFor(300, 5000L)
+
+
+        view.setOnClickListener {
+            startActivity(Intent(this, WorldActivity::class.java))
+        }
     }
 }
