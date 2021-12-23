22b9df608d9db0e2b8869a3a1069a1d7e1ff7dce
Leesin0222 <dragon02229@naver.com>
2021-12-23 11:34:00 +0900

    ➕ :: WorldActivity , xml 파일 생성

---
+++ seald/app/src/main/java/com/yongjincompany/seald/WorldActivity.kt
@@ -0,0 +1,11 @@
+package com.yongjincompany.seald
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class WorldActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_world)
+    }
+}
